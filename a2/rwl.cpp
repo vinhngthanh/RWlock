cddeb42f9ec1ae82801093869848b6d1a389000f
Vinh Nguyen <vinh28062002@gmail.com>
2024-03-20 17:16:09 -0400

    init

---
+++ a2/rwl.cpp
@@ -0,0 +1,30 @@
+class ReadWriteLock {
+	public:
+		ReadWriteLock()
+		{
+			//TODO
+		}
+		
+		void readLock()
+		{
+			//TODO
+		}
+		
+		void readUnlock()
+		{
+			//TODO
+		}
+		
+		void writeLock()
+		{
+			//TODO
+		}
+		
+		void writeUnlock()
+		{
+			//TODO
+		}
+	
+	private:
+		
+};
