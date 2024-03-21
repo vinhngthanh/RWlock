eefcf95697cc069551bb5921b318854e898145c4
Vinh Nguyen <vinh28062002@gmail.com>
2024-03-21 17:43:01 -0400

    attempt 1 success (using mtx for cv.wait)

--- a2/rwl.cpp
+++ a2/rwl.cpp
@@ -1,30 +1,46 @@
+#include <mutex>
+#include <condition_variable>
+#include <memory>
+
+using namespace std;
+
 class ReadWriteLock {
-	public:
-		ReadWriteLock()
-		{
-			//TODO
-		}
-		
-		void readLock()
-		{
-			//TODO
-		}
-		
-		void readUnlock()
-		{
-			//TODO
-		}
-		
-		void writeLock()
-		{
-			//TODO
-		}
-		
-		void writeUnlock()
-		{
-			//TODO
-		}
-	
-	private:
-		
+public:
+  ReadWriteLock(){
+    
+  }
+
+  void readLock() {
+    unique_lock<mutex> lock(*mtx);
+    while (writers > 0) {
+      cv.wait(lock, [&] { return writers == 0; });
+    }
+    readers++;
+  }
+
+  void readUnlock() {
+    lock_guard<mutex> lock(*mtx);
+    readers--;
+    cv.notify_all();
+  }
+
+  void writeLock() {
+    unique_lock<mutex> lock(*mtx);
+    while (readers > 0 || writers > 0) {
+      cv.wait(lock, [&] { return writers == 0 && readers == 0; });
+    }
+    writers++;
+  }
+
+  void writeUnlock() {
+    lock_guard<mutex> lock(*mtx);
+    writers--;
+    cv.notify_all();
+  }
+
+private:
+  unique_ptr<mutex> mtx = make_unique<mutex>();
+  condition_variable cv;
+  int readers = 0;
+  int writers = 0;
 };
